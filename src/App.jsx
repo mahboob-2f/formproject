5c7b62c3eba2fc0552980cb0deab059e41276840
mahboob <mahboob6550@gmail.com>
2025-08-27 18:57:09 +0530

    learnt react tostify

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,83 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { use, useState } from "react"
+// import {toastContainer,toast}
+import {ToastContainer,toast} from 'react-toastify';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    age:"",
+    location:""
+  });
 
+
+  const changeHandler = (e) => {
+    const { name, value, type } = e.target;
+    setFormData(vlaues => ({ ...vlaues, [name]: value }));
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("event prevented");
+    console.log(formData);
+
+    const val = Object.values(formData);
+    let flag = val.every(fun);
+    if(flag){
+      toast.warn("no inputs found !",{
+        position:"top-right",
+        autoClose:2000
+      });
+      return;
+    }
+    function fun(val){
+      return val.trim()==="";
+    }
+
+
+    setFormData({
+      firstName: "",
+      lastName: "",
+      age:"",
+      location:""
+    });
+    toast.success("Form submitted !",{
+      position:"top-right",
+      autoClose:2000
+    });
+
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <form action="" onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="firstName">Enter firstName: </label>
+          <input type="text" id="firstName" name="firstName" placeholder="enter firstname " value={formData.firstName} onChange={changeHandler} />
+        </div>
+        <div>
+          <label htmlFor="lastName">Enter lastName: </label>
+          <input type="text" id="lastName" name="lastName" placeholder="enter lastname" value={formData.lastName} onChange={changeHandler} />
+        </div>
+        <div>
+          <label htmlFor="age">Enter your age: </label>
+          <input type="number" name="age" placeholder="enter age" value={formData.age} onChange={changeHandler} />
+        </div>
+
+        <div>
+          <label htmlFor="location"> Select your locality</label>
+          <select name="location" id="location" value={formData.location} onChange={changeHandler} >
+            <option value="">--select-- </option>
+            <option value="rural">Rural</option>
+            <option value="urban">Urban</option>
+          </select>
+        </div>
+
+
+        <input type="submit" vlaues="submit" />
+      </form>
+      <ToastContainer/>
+    </div>
   )
 }
 
-export default App
+export default App;
